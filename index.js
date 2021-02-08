0481ab112d5367ccbf401d85b987ce6c5cefd1c5
jbrasko <jbrasko0508@gmail.com>
2021-02-08 14:48:12 -0600

    commit tree 7aaff7caeaa51dfcaea06a3af3e2cd1f0ee635a0

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(string="*") {
+    return function(innerParam="special") {
+        return `You are ${string}${innerParam}${string}!`
+    }
+}
+
+let Calculator = {
+    add: function(a, b) {
+        return a + b
+    },
+    subtract: function(a, b) {
+        return a - b
+    },
+    multiply: function(a, b) {
+        return a * b
+    },
+    divide: function(a, b) {
+        return a / b
+    }
+}
+
+let actionApplyer = function(int, funcArray) {
+    let a = int
+
+    for (let i = 0; i < funcArray.length; i++) {
+        a = funcArray[i](a)
+    }
+    return a
+}
